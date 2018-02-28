e163680f7d0f7e48acff894ad32f5870842a9b34
blegrand <benjamin.legrand.externe@capgemini.com>
2018-02-28 15:55:38 +0100

    supprime directive inutilisée.

--- src/app/widget/directives/widget-position.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WidgetPositionDirective } from './widget-position.directive';
-
-describe('WidgetPositionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new WidgetPositionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
