8831b40439e6a4c0f4f555569bd2ac668587ee28
blegrand <benjamin.legrand.externe@capgemini.com>
2018-02-28 12:57:01 +0100

    import des modules + config d'application et routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Maquette WidgetX';
+  viewProps: __esri.MapViewProperties;
+  constructor() {
+    this.viewProps = {
+      zoom: 6,
+      center: [2.2137, 46.2276]
+    };
+  }
 }
---
+++ src/app/routes.ts
@@ -0,0 +1,31 @@
+import {SpacePageComponent} from './layout/pages/space-page/space-page.component';
+import {KitchenSinkComponent} from './layout/components';
+import {Routes} from '@angular/router';
+import {AboutPageComponent} from './layout/pages/about-page/about-page.component';
+import {HelpPageComponent} from './layout/pages/help-page/help-page.component';
+import {HomePageComponent} from './layout/pages';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'kitchen-sink/:componentSelector',
+    component: KitchenSinkComponent,
+  },
+  {
+    path: 'space/:spaceId',
+    component: SpacePageComponent,
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent,
+  },
+  {
+    path: 'help',
+    component: HelpPageComponent,
+  }
+];
+
+export default routes;
