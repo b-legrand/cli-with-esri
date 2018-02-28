b3c7ee51ab11c92fedaee0a583a9b7082409e6cc
blegrand <benjamin.legrand.externe@capgemini.com>
2018-02-28 12:55:00 +0100

    module library : étagère de composants.

---
+++ src/app/library/components/widget-toc/widget-toc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'widget-toc',
+  templateUrl: './widget-toc.component.html',
+  styleUrls: ['./widget-toc.component.scss']
+})
+export class WidgetTocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
