db4c5df280e1d5c5652029cbdf1cd84a9c63d9b1
blegrand <benjamin.legrand.externe@capgemini.com>
2018-03-05 10:50:31 +0100

    configuration de jest.

--- src/jest.ts
+++ src/jest.ts
@@ -1,18 +1,17 @@
-/*import 'jest-preset-angular';
+import 'jest-preset-angular';
 
 const mock = () => {
-    let storage = {};
-    return {
-      getItem: key => key in storage ? storage[key] : null,
-      setItem: (key, value) => storage[key] = value || '',
-      removeItem: key => delete storage[key],
-      clear: () => storage = {},
-    };
+  let storage = {};
+  return {
+    getItem: key => key in storage ? storage[key] : null,
+    setItem: (key, value) => storage[key] = value || '',
+    removeItem: key => delete storage[key],
+    clear: () => storage = {},
   };
-  
-  Object.defineProperty(window, 'localStorage', {value: mock()});
-  Object.defineProperty(window, 'sessionStorage', {value: mock()});
-  Object.defineProperty(window, 'getComputedStyle', {
-    value: () => ['-webkit-appearance']
-  });
-  */
+};
+
+Object.defineProperty(window, 'localStorage', {value: mock()});
+Object.defineProperty(window, 'sessionStorage', {value: mock()});
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => ['-webkit-appearance']
+});
