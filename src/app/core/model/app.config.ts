12d16068aa4ee9aa6ebdc2db8895394d363df4cb
Benjamin Legrand <b.legrand@groupeonepoint.com>
2018-03-15 14:24:44 +0100

    couleurs en constantes.

--- src/app/core/model/app.config.ts
+++ src/app/core/model/app.config.ts
@@ -1,19 +1,26 @@
 import { InjectionToken } from '@angular/core';
+import { SncfColor } from '../constants/sncf-color.enum';
 
 /**
  * Constante permettant d'éviter les conflits de nom lors de l'injection.
  */
 export let APP_CONFIG = new InjectionToken('app.config');
 
+/**
+ * Configuration de base d'une application socle.
+ */
 export interface AppConfig {
+
   /**
    * Nom de l'application, affiché en titre et dans le header.
    */
   appName: string;
+
   /**
    * Couleur SNCF de l'application.
    */
   themeColor: string;
+
   /**
    * Version de l'api arcgis.
    */
@@ -26,18 +33,6 @@ export interface AppConfig {
 export const DEFAULT_APP_CONFIG: AppConfig = {
   appName: 'Maquette widget-container',
   apiVersion: '4.6',
-  themeColor: 'rgb(0, 136, 206)',
-//    themeColor: 'rgb(110, 30, 120)',  // violet
-//    themeColor: 'rgb(255, 182, 18)',  // jaune-safran
-//    themeColor: 'rgb(0, 136, 206)',     // bleu-primaire
-//    themeColor: 'rgb(60, 55, 50)',    // carbone
-//    themeColor: 'rgb(213, 43, 30)',   // rouge-assistance
-//    themeColor: 'rgb(161, 0, 107)',   // prune
-//    themeColor: 'rgb(210, 225, 0)',   // vert-anis
-//    themeColor: 'rgb(255, 255, 255)', // blanc
-//    themeColor: 'rgb(205, 0, 55)',    // framboise
-//    themeColor: 'rgb(130, 190, 0)',   // vert-pomme
-//    themeColor: 'rgb(224, 82, 6)',    // orange
-//    themeColor: 'rgb(0, 154, 166)',   // bleu-canard
+  themeColor: SncfColor.VertPomme as string,
 };
 
