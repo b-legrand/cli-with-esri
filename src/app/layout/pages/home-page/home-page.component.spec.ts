455d78729dcba368120ec4d58edb075a9c6ff6da
blegrand <benjamin.legrand.externe@capgemini.com>
2018-03-19 09:27:24 +0100

    Pages stupides (home/not found)

--- src/app/layout/pages/home-page/home-page.component.spec.ts
+++ src/app/layout/pages/home-page/home-page.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePageComponent } from './home-page.component';
+import { DebugElement } from '@angular/core';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,9 +19,16 @@ describe('HomePageComponent', () => {
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display <<Hello World>>', () => {
+    expect(debugElement.nativeElement).toBeTruthy();
+    expect(debugElement.nativeElement.textContent).toContain('Hello World!');
+  });
+
 });
--- src/app/layout/pages/page-not-found/page-not-found.component.spec.ts
+++ src/app/layout/pages/page-not-found/page-not-found.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import {DebugElement} from '@angular/core';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,9 +19,17 @@ describe('PageNotFoundComponent', () => {
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain 404 error message', () => {
+    expect(debugElement).toBeTruthy();
+    expect(debugElement.nativeElement).toBeTruthy();
+    expect(debugElement.nativeElement.textContent).toContain('Page not found');
+  });
+
 });
