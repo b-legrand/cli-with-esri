e6b7d184019520964f5b66e179c8bf6e594bf9a2
blegrand <benjamin.legrand.externe@capgemini.com>
2018-03-29 10:26:50 +0200

    texte en gras + activation toc.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -72,3 +72,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
