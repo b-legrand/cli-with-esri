0a28e8bc39f4a055c85ef8fe3ba347d71ab31443
Benjamin Legrand <b.legrand@groupeonepoint.com>
2018-04-03 16:11:25 +0200

    prettier lorem-ipsum

--- src/app/layout/directives/lorem-ipsum.directive.spec.ts
+++ src/app/layout/directives/lorem-ipsum.directive.spec.ts
@@ -1,22 +1,21 @@
-import { LoremIpsumDirective } from './lorem-ipsum.directive';
-import { Component, DebugElement, Input } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { LoremIpsumDirective } from "./lorem-ipsum.directive";
+import { Component, DebugElement, Input } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 
 // composant de test de la directive
 @Component({
-  template: `<div [loremIpsum]="wordCount"></div>`
+  template: `<div [loremIpsum]="wordCount"></div>`,
 })
 class TestLoremIpsumComponent {
   @Input() public wordCount: number;
 }
 
-describe('LoremIpsumDirective', () => {
-
+describe("LoremIpsumDirective", () => {
   let component: TestLoremIpsumComponent;
   let fixture: ComponentFixture<TestLoremIpsumComponent>;
   let debugElement: DebugElement;
-  const expectedText = 'Lorem ipsum';
+  const expectedText = "Lorem ipsum";
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,32 +23,31 @@ describe('LoremIpsumDirective', () => {
     });
     fixture = TestBed.createComponent(TestLoremIpsumComponent);
     component = fixture.componentInstance;
-    debugElement = fixture.debugElement.query(By.css('div'));
+    debugElement = fixture.debugElement.query(By.css("div"));
   });
 
-  it('doit avoir du contenu généré dans innerHTML', () => {
+  it("doit avoir du contenu généré dans innerHTML", () => {
     fixture.detectChanges();
     const divElement: HTMLElement = debugElement.nativeElement;
     expect(divElement.innerHTML).toBeDefined();
     expect(divElement.innerHTML).toContain(expectedText);
   });
 
-  it('le nombre de mots peut être passé en entrée.', () => {
+  it("le nombre de mots peut être passé en entrée.", () => {
     component.wordCount = 10;
     fixture.detectChanges();
     const divElement: HTMLElement = debugElement.nativeElement;
     expect(divElement.innerHTML).toBeDefined();
     expect(divElement.innerHTML).toContain(expectedText);
-    expect(divElement.innerHTML.split(' ').length).toBeLessThan(11);
+    expect(divElement.innerHTML.split(" ").length).toBeLessThan(11);
   });
 
-  it('un nombre de mots supérieur au texte de base génère du texte aléatoire.', () => {
+  it("un nombre de mots supérieur au texte de base génère du texte aléatoire.", () => {
     component.wordCount = 300;
     fixture.detectChanges();
     const divElement: HTMLElement = debugElement.nativeElement;
     expect(divElement.innerHTML).toBeDefined();
     expect(divElement.innerHTML).toContain(expectedText);
-    expect(divElement.innerHTML.split(' ').length).toBeGreaterThan(280);
+    expect(divElement.innerHTML.split(" ").length).toBeGreaterThan(280);
   });
-
 });
